bbf963c00f43005362e402b66d81c6befc582d06
samsong0814 <songws@mail.bnu.edu.cn>
2017-08-01 09:51:01 +0800

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,16 @@
+setwd("C:/Users/songweishan/Desktop/R")
+if(!exists("NEI")){ 
+        NEI <- readRDS("summarySCC_PM25.rds") 
+} 
+if(!exists("SCC")){ 
+        SCC <- readRDS("Source_Classification_Code.rds") 
+}
+
+
+subsetNEI  <- NEI[NEI$fips=="24510", ]
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
+
+png('plot2.png')
+barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' in the Baltimore City, MD emissions at various years'))
+dev.off()
